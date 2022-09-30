6e031b3a40aa764b6c3fd9b7ca7c1b075792ec4f
sathyagimenes <sathya.gimenes@gmail.com>
2022-09-30 14:06:05 -0300

    folder creation and start of .h

---
+++ includes/so_long.h
@@ -0,0 +1,20 @@
+
+
+#ifndef SO_LONG_H
+
+// includes
+#include <mlx.h>
+#include <libft.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SO_LONG_H
+
+// Macros
+
+// Structs
+
+// Functions declarations
