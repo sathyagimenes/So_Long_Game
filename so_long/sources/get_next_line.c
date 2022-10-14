539fb240dc591ec4cdc414f9ead64aef0d2ef92e
sde-cama <sde-cama@student.42sp.org.br>
2022-10-14 17:35:45 +0200

    trying to fix gnl

--- so_long/sources/get_next_line.c
+++ so_long/sources/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 21:18:17 by sde-cama          #+#    #+#             */
-/*   Updated: 2022/10/12 22:39:29 by sde-cama         ###   ########.fr       */
+/*   Updated: 2022/10/14 17:24:39 by sde-cama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,33 @@
 char	*ft_read_line(int fd, char *storage);
 char	*split_line(char *storage);
 char	*reset_storage(char *storage, char *tmp_storage);
-char	*ft_getline(int len, char *read);
+char	*make_line(char **line, char **read_buffer);
+int	ft_strchr2(char *s, char c);
 
 int	get_next_line(int fd, char **line)
 {
-	static char	*storage[1023];
+	static char	*storage;
 	char		*current_line;
 	char		*temp_storage;
 
-	if ((fd < 0) || (BUFFER_SIZE <= 0) || (fd > 1024))
+	if ((fd < 0) || (BUFFER_SIZE <= 0))
 		return (-1);
-	if (storage[fd] == NULL)
+	if (storage == NULL)
 		temp_storage = ft_strdup("");
 	else
-		temp_storage = ft_strdup(storage[fd]);
+		temp_storage = ft_strdup(storage);
 	temp_storage = ft_read_line(fd, temp_storage);
 	if (temp_storage == NULL)
 	{
 		free(temp_storage);
-		free(storage[fd]);
-		storage[fd] = NULL;
+		free(storage);
+		storage = NULL;
 		return (0);
 	}
 	current_line = split_line(temp_storage);
-	storage[fd] = reset_storage(storage[fd], temp_storage);
+	storage = reset_storage(storage, temp_storage);
 	free(temp_storage);
-	*line = ft_getline(ft_strlen(current_line), current_line);
+	current_line = make_line(line, &current_line);
 	free(current_line);
 	return (1);
 }
@@ -104,18 +105,201 @@ char	*reset_storage(char *storage, char *tmp_storage)
 	return (storage);
 }
 
-char	*ft_getline(int len, char *read)
+char	*make_line(char **line, char **read_buffer)
 {
-	char	*line;
-	int		i;
+	char	*temp;
+	int		end_line;
 
-	line = malloc(len + 1);
-	i = 0;
-	while (i < len)
+	temp = NULL;
+	end_line = ft_strchr2(*read_buffer, '\n');
+	if (end_line != -1)
 	{
-		line[i] = read[i];
+		read_buffer[0][end_line] = '\0';
+		*line = ft_strdup(*read_buffer);
+		end_line++;
+		temp = ft_strdup(*read_buffer + end_line);
+	}
+	else
+		*line = ft_strdup(*read_buffer);
+	free(*read_buffer);
+	read_buffer = NULL;
+	if (!line || !temp)
+		return (NULL);
+	return (temp);
+}
+
+
+// #include "so_long.h"
+
+int	ft_strchr2(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (-1);
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (i);
 		i++;
 	}
-	line[i] = '\0';
-	return (line);
+	if (s[i] == c)
+		return (i);
+	return (-1);
 }
+
+// size_t	ft_strlen2(char *s)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	if (!s)
+// 		return (0);
+// 	while (s[i])
+// 		i++;
+// 	return (i);
+// }
+
+// void	ft_strcopy2(char *dst, char *src, size_t len)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	if (!src || len == 0)
+// 		return ;
+// 	while (src[i] && i < (len - 1))
+// 	{
+// 		dst[i] = src[i];
+// 		i++;
+// 	}
+// 	dst[i] = '\0';
+// 	return ;
+// }
+
+// char	*ft_strjoin2(char *dst, char *src, size_t len)
+// {
+// 	char	*new;
+
+// 	if (!dst && !src)
+// 		return (NULL);
+// 	new = (char *)calloc(sizeof(char), (ft_strlen2(dst) + len + 1));//aqui
+// 	if (!new)
+// 		return (NULL);
+// 	ft_strcopy2(new, dst, ft_strlen2(dst) + 1);
+// 	ft_strcopy2((new + ft_strlen2(dst)), src, len + 1);
+// 	if (dst)
+// 		free(dst);
+// 	return (new);
+// }
+
+// char	*ft_strdup2(char *str)
+// {
+// 	size_t	size_str;
+// 	char	*new_str;
+
+// 	size_str = ft_strlen2(str);
+// 	new_str = (char *)calloc(sizeof(char), size_str + 1);
+// 	if (str == NULL || !new_str)
+// 		return (NULL);
+// 	ft_strcopy2(new_str, str, size_str + 1);
+// 	return (new_str);
+// }
+
+// void	*ft_calloc2(size_t nmemb, size_t size)
+// {
+// 	void	*pointer;
+// 	char	*set;
+// 	size_t	i;
+
+// 	i = 0;
+// 	pointer = malloc(nmemb * size);
+// 	if (!pointer)
+// 		return (NULL);
+// 	set = (char *)pointer;
+// 	while (i < size)
+// 	{
+// 		set[i] = '\0';
+// 		i++;
+// 	}
+// 	return (pointer);
+// }
+
+// static char	*make_line(char **line, char **read_buffer)
+// {
+// 	char	*temp;
+// 	int		end_line;
+
+// 	temp = NULL;
+// 	end_line = ft_strchr2(*read_buffer, '\n');
+// 	if (end_line != -1)
+// 	{
+// 		read_buffer[0][end_line] = '\0';
+// 		*line = ft_strdup2(*read_buffer);
+// 		end_line++;
+// 		temp = ft_strdup2(*read_buffer + end_line);
+// 	}
+// 	else
+// 		*line = ft_strdup2(*read_buffer);
+// 	free(*read_buffer);
+// 	read_buffer = NULL;
+// 	if (!line || !temp)
+// 		return (NULL);
+// 	return (temp);
+// }
+
+// int	read_and_join(int fd, char **read_buffer, char *line_buffer, int *bytes)
+// {
+// 	char	*temp;
+// 	size_t	size;
+
+// 	while (*bytes && ft_strchr2(*read_buffer, '\n') == -1)
+// 	{
+// 		*bytes = read(fd, line_buffer, BUFFER_SIZE);
+// 		if (*bytes < 0 || BUFFER_SIZE < *bytes)
+// 		{
+// 			free(line_buffer);
+// 			return (-1);
+// 		}
+// 		if (*bytes)
+// 		{
+// 			temp = NULL;
+// 			if (*bytes < BUFFER_SIZE)
+// 				temp = ft_strjoin2(temp, line_buffer, *bytes);
+// 			else
+// 				temp = ft_strjoin2(temp, line_buffer, BUFFER_SIZE + 1);
+// 			size = ft_strlen2(*read_buffer);
+// 			*read_buffer = ft_strjoin2(*read_buffer, temp, (BUFFER_SIZE + size));
+// 			free(temp);
+// 		}
+// 	}
+// 	free(line_buffer);
+// 	return (1);
+// }
+
+// int	get_next_line(int fd, char **line)
+// {
+// 	static char	*read_buffer;
+// 	char		*line_buffer;
+// 	int			bytes;
+// 	int			already_read;
+
+// 	bytes = 1;
+// 	if (fd < 0 || fd > RLIMIT_NOFILE || !(line) || BUFFER_SIZE <= 0)
+// 		return (-1);
+// 	line_buffer = (char *)ft_calloc2(sizeof(char), BUFFER_SIZE + 1);
+// 	if (!line_buffer)
+// 		return (-1);
+// 	if (!read_buffer)
+// 		read_buffer = ft_strdup2("");
+// 	already_read = read_and_join(fd, &read_buffer, line_buffer, &bytes);
+// 	if (already_read == -1)
+// 		return (-1);
+// 	read_buffer = make_line(line, &read_buffer);
+// 	if (!bytes)
+// 	{
+// 		free(read_buffer);
+// 		return (0);
+// 	}
+// 	return (1);
+// }
--- so_long/sources/read_map.c
+++ so_long/sources/read_map.c
@@ -1,166 +1,270 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   read_map.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/07 23:20:58 by sde-cama          #+#    #+#             */
-/*   Updated: 2022/10/14 00:33:16 by sde-cama         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   read_map.c                                         :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2022/10/07 23:20:58 by sde-cama          #+#    #+#             */
+// /*   Updated: 2022/10/14 15:49:57 by sde-cama         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
+
+// #include "so_long.h"
+
+// int	set_map_length(char *argv_1, t_program *program);
+// int	valid_read(char *argv_1);
+// int	save_map(char *argv_1, t_program *program);
+// int	verify_walls(t_program *program);
+
+// int	read_map(char *argv_1, t_program *program)
+// {
+// 	// int		fd;
+// 	// char	*line;
+// 	// char	*map_single_line;
+
+// 	if (!valid_read(argv_1))
+// 		return (print_map_error(INVALID_MAP));
+// 	if (!set_map_length(argv_1, program))
+// 		return (print_map_error(INVALID_SIZE));
+// 	if (!save_map(argv_1, program))
+// 	{
+// 		free_grid(program);
+// 		return (print_map_error(INVALID_MAP));
+// 	}
+// 	if (!verify_walls(program))
+// 	{
+// 		free_grid(program);
+// 		return (print_map_error(INVALID_MAP));
+// 	}
+// 	// if (!verify_valid_entities_for(program))
+// 	// {
+// 	// 	free_grid(program);
+// 	// 	return (print_so_long_error(ERR_INVALID_ENTITIES));
+// 	// }
+// 	return (SUCCESS);
+// }
+
+// int	set_map_length(char *argv_1, t_program *program)
+// {
+// 	int		fd;
+// 	int		gnl;
+// 	int		nrows;
+// 	int		ncolumns;
+// 	char	*line;
+
+// 	gnl = 1;
+// 	nrows = 0;
+// 	ncolumns = 0;
+// 	fd = open(argv_1, O_RDONLY);
+// 	while (gnl)
+// 	{
+// 		gnl = get_next_line(fd, &line);
+// 		nrows++;
+// 		if (nrows == 1)
+// 			ncolumns = ft_strlen(line);
+// 		if (gnl == 1)
+// 			free(line);
+// 	}
+// 	close(fd);
+// 	if (nrows < 4 || ncolumns < 4)
+// 		return (FAIL);
+// 	program->row_qnty = nrows;
+// 	program->column_qnty = ncolumns;
+// 	return (SUCCESS);
+// }
+
+// int	save_map(char *argv_1, t_program *program)
+// {
+// 	int		fd;
+// 	int		i;
+// 	int		gnl;
+// 	int		nrows;
+
+// 	i = 0;
+// 	gnl = 1;
+// 	nrows = program->row_qnty;
+// 	fd = open(argv_1, O_RDONLY);
+// 	program->map_grid = (char **)ft_calloc(sizeof(char *), nrows + 1);
+// 	while (gnl)
+// 	{
+// 		gnl = get_next_line(fd, &program->map_grid[i]);
+// 		i++;
+// 	}
+// 	close(fd);
+// 	return (SUCCESS);
+// }
+
+// int	valid_read(char *argv_1)
+// {
+// 	int		fd;
+// 	char	*line;
+// 	int		gnl;
+
+// 	fd = open(argv_1, O_RDONLY);
+// 	gnl = get_next_line(fd, &line);
+// 	if (gnl < 0)
+// 		return (FAIL);
+// 	free(line);
+// 	close(fd);
+// 	return (SUCCESS);
+// }
+
+// int	verify_walls(t_program *program)
+// {
+// 	int	x;
+// 	int	y;
+// 	//char	**grid;
+
+// 	x = 0;
+// 	//grid = program->map_grid;
+// 	while (x < program->row_qnty)
+// 	{
+// 		y = 0;
+// 		while (y < program->column_qnty)
+// 		{
+// 			//ft_printf("%d,%d: %c ", x,y, grid[x][y]);
+// 			ft_printf("%d:%d", program->column_qnty,program->row_qnty);
+// 			y++;
+// 		}
+// 		x++;
+// 	}
+// 	return (SUCCESS);
+// }
+
+
 
 #include "so_long.h"
 
-int	set_map_length(char *argv_1, t_program *program);
-int	valid_read(char *argv_1);
-int	save_map(char *argv_1, t_program *program);
-int	verify_walls(t_program *program);
-
-int	read_map(char *argv_1, t_program *program)
-{
-	// int		fd;
-	// char	*line;
-	// char	*map_single_line;
-
-	if (!valid_read(argv_1))
-		return (print_map_error(INVALID_MAP));
-	if (!set_map_length(argv_1, program))
-		return (print_map_error(INVALID_SIZE));
-	if (!save_map(argv_1, program))
-	{
-		free_grid(program);
-		return (print_map_error(INVALID_MAP));
-	}
-	if (!verify_walls(program))
-	{
-		free_grid(program);
-		return (print_map_error(INVALID_MAP));
-	}
-	// if (!verify_valid_entities_for(program))
-	// {
-	// 	free_grid(program);
-	// 	return (print_so_long_error(ERR_INVALID_ENTITIES));
-	// }
-	return (SUCCESS);
-}
-
-int	set_map_length(char *argv_1, t_program *program)
+static int	read_map_nrows(char *argv_1)
 {
 	int		fd;
+	int		counter;
 	int		gnl;
-	int		nrows;
-	int		ncolumns;
 	char	*line;
 
+	counter = 0;
 	gnl = 1;
-	nrows = 0;
-	ncolumns = 0;
 	fd = open(argv_1, O_RDONLY);
+	if (fd < 0)
+		return (0);
 	while (gnl)
 	{
 		gnl = get_next_line(fd, &line);
-		nrows++;
-		if (nrows == 1)
-			ncolumns = ft_strlen(line);
-		if (gnl == 1)
-			free(line);
+		if (gnl < 0)
+		{
+			return (FAIL);
+		}
+		counter++;
+		//free(line);
 	}
 	close(fd);
-	if (nrows < 4 || ncolumns < 4)
-		return (FAIL);
-	program->row_qnty = nrows;
-	program->column_qnty = ncolumns;
-	return (SUCCESS);
+	return (counter);
 }
 
-int	save_map(char *argv_1, t_program *program)
+static int	parse_map_for(int nrows, char *argv_1, t_program *program)
 {
 	int		fd;
 	int		i;
 	int		gnl;
-	int		nrows;
 
 	i = 0;
 	gnl = 1;
-	nrows = program->row_qnty;
-	fd = open(argv_1, O_RDONLY);
 	program->map_grid = (char **)ft_calloc(sizeof(char *), nrows + 1);
+	if (!program->map_grid)
+		return (FAIL);
+	fd = open(argv_1, O_RDONLY);
 	while (gnl)
 	{
 		gnl = get_next_line(fd, &program->map_grid[i]);
+		if (gnl < 0)
+		{
+			free_grid(program);
+			return (FAIL);
+		}
+		ft_printf("%s\n", program->map_grid[i]);
 		i++;
 	}
+	//ensure_no_gnl_leaks(program, &nrows);
 	close(fd);
+	program->row_qnty = nrows;
+	program->column_qnty = ft_strlen(program->map_grid[0]);
+	ft_printf("%d:%d\n", program->column_qnty,program->row_qnty); //aqui
 	return (SUCCESS);
 }
 
-int	valid_read(char *argv_1)
-{
-	int		fd;
-	char	*line;
-	int		gnl;
-
-	fd = open(argv_1, O_RDONLY);
-	gnl = get_next_line(fd, &line);
-	if (gnl < 0)
-		return (FAIL);
-	free(line);
-	close(fd);
-	return (SUCCESS);
-}
-
-int	verify_walls(t_program *program)
+static int	verify_walls_for(t_program *program)
 {
 	int	i;
-	int	j;
-	int	ncolumn;
-	int	nrow;
-	ncolumn = program->column_qnty;
-	nrow = program->row_qnty;
 
 	i = 0;
 	while (i < program->column_qnty)
 	{
-		j = 0;
-		while (j < program->row_qnty)
-		{
-			if (i == 0 || j == 0
-				|| i == ncolumn - 1 || j == nrow - 1)
-			{
-				if (program->map_grid[i][j] != '1')
-			 		ft_printf("%c", program->map_grid[i][j]);
-			}
-			j++;
-		}
+		if (program->map_grid[0][i] != WALL
+			|| program->map_grid[program->row_qnty - 1][i] != WALL)
+			return (FAIL);
 		i++;
 	}
-	// int	i;
-	// int	j;
+	i = 0;
+	while (i < program->row_qnty - 1)
+	{
+		if (program->map_grid[i][0] != WALL
+			|| program->map_grid[i][program->column_qnty - 1] != WALL
+			|| ft_strlen(program->map_grid[i]) != ft_strlen(program->map_grid[i + 1]))
+			return (FAIL);
+		i++;
+	}
+	return (SUCCESS);
+}
 
-	// i = 0;
-	// while (i < program->column_qnty)
-	// {
-	// 	j = 0;
-	// 	while (j < program->row_qnty)
-	// 	{
-	// 		ft_printf("%c", program->map_grid[i][j]);
+static int	verify_valid_entities_for(t_program *program)
+{
+	int	row;
+	int	col;
 
-	// 		// if (program->map_grid[0][j - 1] != WALL)
-	// 		// 	ft_printf("%c", program->map_grid[0][j - 1]);
-	// 		// if (program->map_grid[0][j] != WALL)
-	// 		// 	return (FAIL);
-	// 		// if (program->map_grid[i][0] != WALL || program->map_grid[i][program->column_qnty - 1] != WALL)
-	// 		// 	return (FAIL);
-	// 		// if (j == 0 && program->map_grid[i][j] != WALL)
-	// 		// 	return (FAIL);
-	// 		// if (j == (program->row_qnty - 1) && program->map_grid[i][j] != WALL)
-	// 		// 	return (FAIL);
-	// 		// if (ft_strlen(program->map_grid[i]) != ft_strlen(program->map_grid[i + 1]))
-	// 		// 	return (FAIL);
-	// 		j++;
-	// 	}
-	// 	i++;
-	// }
+	row = 1;
+	while (row <= program->row_qnty - 2)
+	{
+		col = 1;
+		while (col <= program->column_qnty - 2)
+		{
+			if (program->map_grid[row][col] != COLLECTIBLE
+				&& program->map_grid[row][col] != PLAYER_POSITION
+				&& program->map_grid[row][col] != EXIT
+				&& program->map_grid[row][col] != EMPTY_SPACE
+				&& program->map_grid[row][col] != WALL)
+			{
+				free_grid(program);
+				return (print_map_error(INVALID_MAP));
+			}
+			col++;
+		}
+		row++;
+	}
+	return (SUCCESS);
+}
+
+int	read_map(char *argv_1, t_program *program)
+{
+	int	nrows;
+
+	nrows = read_map_nrows(argv_1);
+	if (!nrows)
+		return (print_map_error(INVALID_MAP));
+	if (!parse_map_for(nrows, argv_1, program))
+	{
+		free_grid(program);
+		return (print_map_error(INVALID_MAP));
+	}
+	if (!verify_walls_for(program))
+	{
+		free_grid(program);
+		return (print_map_error(INVALID_MAP));
+	}
+	if (!verify_valid_entities_for(program))
+	{
+		free_grid(program);
+		return (print_map_error(INVALID_MAP));
+	}
 	return (SUCCESS);
 }
